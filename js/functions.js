6c86ae200ee525e2c2c8eab13b7dda2c76ff807e
walshyb <walshb1@hawkmail.newpaltz.edu>
2014-11-11 13:49:15 -0500

    Login / Signup functionality; logout button
    Signing up updates Parse database; login gets credentials from Parse
    database; added a logout button

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 Parse.initialize("CteLLpCfDDSF940eJkYbdSakKYecqRKhB55PtKac", "Z0PHT9eOK69oLEKMHzKxDUtL9Lj59165nxaRru1O");
 
-$("#signup-form").submit(function(){
+/*Signs user up and then logs in*/
+$(document).on("submit", "#signup-form", function(){
     var user = new Parse.User();
     var signup_username = $('#signup-username').val();
     var signup_email = $('#signup-email').val();
@@ -12,18 +13,37 @@ $("#signup-form").submit(function(){
     
     user.signUp(null, {
       success: function(user) {
-          alert("Successful");
+        Parse.User.logIn(signup_username, signup_password, {
+            success: function(user) {
+                alert("Sign in successful");
+            },
+            error: function(user, error) {
+                alert("Error: " + error.code + " " + error.message);
+            } 
+        });
       },
       error: function(user, error) {
         alert("Error: " + error.code + " " + error.message);
       }
     });
-    
-    Parse.User.logIn(signup_username, signup_password, {
-	  success: function(user) {
-	  },
-	  error: function(user, error) {
-	    alert("Error: " + error.code + " " + error.message);
-	  } 
-	});
+});
+
+/*Logs user in*/
+$(document).on("submit", "#login-form", function(){
+    var login_username = $('#login-username').val();;
+    var login_password = $('#login-password').val();;
+    Parse.User.logIn(login_username, login_password, {
+        success: function(user) {
+            alert("Sign in successful");
+        },
+        error: function(user, error) {
+            alert("Error: " + error.code + " " + error.message);
+        } 
+    });
+});
+
+/*Signs user out*/
+$(document).on("click", "#logout-btn", function(){
+    alert(Parse.User.current().getUsername());
+    Parse.User.logOut();
 });
